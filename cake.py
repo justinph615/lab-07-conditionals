c65264347136aa214fa42509f9ccce27e4676fde
justinph615 <justinph615@gmail.com>
2021-03-22 16:42:53 -0400

    third commit

---
+++ cake.py
@@ -0,0 +1,22 @@
+
+
+cakeorbust = input("Do you want cake? (yes/no)")
+
+if cakeorbust == "yes" or cakeorbust == "YES" or cakeorbust == "y":
+
+	print("Have some cake!")
+
+
+elif cakeorbust == "no" or cakeorbust == "NO" or cakeorbust == "n":
+
+	print("No cake for you!")
+
+
+else: 
+		
+	print("Sorry, I don't understand.")
+
+
+		
+
+
