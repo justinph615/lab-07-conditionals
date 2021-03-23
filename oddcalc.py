47418a8e85a8a8e21ac78a5d1c13ddaf3612b13a
justinph615 <justinph615@gmail.com>
2021-03-23 13:58:49 -0400

    fixed oddcalc.py

--- oddcalc.py
+++ oddcalc.py
@@ -1,19 +1,19 @@
 
 
-firstnum = int(float(input("Give me a number: ")))
-secondnum = int(float(input("Give me another number: ")))
-operation = float(input("What operation do you want? (mul/div/mod) "))
+firstnum = float(input("Give me a number: "))
+secondnum = float(input("Give me another number: "))
+operation = input("What operation do you want? (mul/div/mod) ")
 
 
 if operation == "mul"or operation == "MUL":
 		
-			print(float(firstnum) * float(secondnum))
+			print(firstnum * secondnum)
 
 
 elif operation == "div" or operation == "DIV":
 
 
-		print(int(firstnum) / int(secondnum))
+		print(firstnum / secondnum)
 
 
 		 
@@ -21,7 +21,7 @@ elif operation == "mod" or operation == "MOD":
 
 
 		
-		print(int(firstnum) % int(secondnum))
+		print(firstnum % secondnum)
 
 
 else: 
